ae315d512dfbbfa2ac74f96175eb51d3057dd652
tracer-ics2023 <tracer@njuics.org>
2024-03-25 13:35:05 +0800

    >  compile NEMU xxxxxxxxx 赵治 Linux zz-Desktop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:35:05 up 52 min,  1 user,  load average: 0.23, 0.21, 0.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,11 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int test(char *args) {
+  printf("test");
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,7 +67,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "t", "test", test },
   /* TODO: Add more commands */
 
 };

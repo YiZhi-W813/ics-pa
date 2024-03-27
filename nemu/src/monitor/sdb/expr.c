530827c05fd0d25a17de5dca467f2f4eb8c3cbde
tracer-ics2023 <tracer@njuics.org>
2024-03-27 14:54:25 +0800

    >  compile NEMU xxxxxxxxx 赵治 Linux zz-Desktop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:25 up  3:12,  1 user,  load average: 0.26, 0.36, 0.45

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -415,6 +415,7 @@ word_t expr(char *e, bool *success) {
 
     for(int i = 0 ; i < tokens_len ; i ++)  //初始化指针解引用
     {
+      printf("test");
       if((tokens[i].type == '*' && i > 0 && tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG && tokens[i+1].type == TK_NUM )
         ||(tokens[i].type == '*' && i > 0 && tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG && tokens[i+1].type == TK_HEX )
         ||(tokens[i].type == '*' && i == 0)){

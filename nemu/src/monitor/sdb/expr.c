ba73fafb65509d370e6274d8e9cb28bc66d0edb2
tracer-ics2023 <tracer@njuics.org>
2024-03-27 19:17:52 +0800

    >  compile NEMU xxxxxxxxx 赵治 Linux zz-Desktop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:17:52 up 28 min,  1 user,  load average: 0.81, 0.46, 0.35

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -420,9 +420,9 @@ word_t expr(char *e, bool *success) {
         ||(tokens[i].type == TK_MUL && i == 0)){
         printf("tokens[%d].type = %d .str=%s\n",i+1,tokens[i+1].type,tokens[i+1].str);
         tokens[i].type = TK_NOTYPE;
-        printf("test\n");
         long tmp = atol(tokens[i+1].str);
-        printf("tmp = %ld",tmp);
+        printf("test\n");
+//        printf("tmp = %ld",tmp);
         uintptr_t point = (uintptr_t)tmp;
         printf("poinr = %lu",point);
         int value = *((int*)point);

6b48ddb55cfa8fdf0ace6a6358e3c786d04281a5
tracer-ics2023 <tracer@njuics.org>
2024-03-28 14:20:21 +0800

    >  compile NEMU xxxxxxxxx 赵治 Linux zz-Desktop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:21 up 58 min,  1 user,  load average: 0.59, 0.82, 0.83

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,18 +1,3 @@
-/***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,74 +6,196 @@
 #include <string.h>
 
 // this should be enough
-static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+static char buf[65536] = "";
+static char code_buf[65536];
 static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%d\", result); "
-"  return 0; "
-"}";
-
-int index_buf = 0;
-int choose(int n){
+    "#include <stdio.h>\n"
+    "int main() { "
+    "  unsigned result = %s; "
+    "  printf(\"%%u\", result); "
+    "  return 0; "
+    "}";
+// TODO: implement these functions: choose, gen_rand_op, gen_num, gen_rand_expr
+static inline uint32_t choose(uint32_t n)
+{
   return rand() % n;
 }
 
-void gen(char c){
-  char cha_buffer[2] = {c, '\0'};
-  strcat(buf, cha_buffer);
+static inline void gen_rand_op()
+{
+  switch (choose(8))
+  {
+  case 0:
+    sprintf(buf + strlen(buf), "%s", "+");
+    break;
+  case 1:
+    sprintf(buf + strlen(buf), "%s", "-");
+    break;
+  case 2:
+    sprintf(buf + strlen(buf), "%s", "*");
+    break;
+  case 3:
+    sprintf(buf + strlen(buf), "%s", "/");
+    break;
+  case 4:
+    sprintf(buf + strlen(buf), "%s", "&&");
+    break;
+  case 5:
+    sprintf(buf + strlen(buf), "%s", "||");
+    break;
+  case 6:
+    sprintf(buf + strlen(buf), "%s", "==");
+    break;
+  case 7:
+    sprintf(buf + strlen(buf), "%s", "!=");
+    break;
+  }
+  return;
 }
 
-char gen_rand_op(){
-  switch (choose(4)){
+static inline void gen_blank()
+{
+  int x = rand() % 4;
+  sprintf(buf + strlen(buf), "%*s", x, "");
+}
+
+static inline void gen_num()
+{
+  int x = rand() % 1000;
+  sprintf(buf + strlen(buf), "%d", x);
+  /*
+  switch(choose(2))
+  {
     case 0:
-      gen('+');
-      return '+';
+      sprintf(buf + strlen(buf), "%d", x);
+      break;
     case 1:
-      gen('-');
-      return '-';
-    case 2:
-      gen('*');
-      return '*';
-    case 3:
-      gen('/');
-      return '/';
+      sprintf(buf+ strlen(buf), "%#x", x);
+      break;
   }
-  return ' ';
+  */
+  return;
 }
 
+static char *cd_format =
+    "#include <stdio.h>\n"
+    "int main() { "
+    "  int result = %s; "
+    "  printf(\"%%d\", result); "
+    "  return 0; "
+    "}";
 
-uint32_t gen_num(){
-  char num_buffer[1024];
-  num_buffer[0] = '\0';
-  uint32_t number = rand() % 100 + 1;
-  sprintf(num_buffer ,"%d", number);
-  strcat(buf, num_buffer);
-  return number;
-}
+static int check_expr(char *buf, int x)
+{
+  sprintf(code_buf, cd_format, buf);
 
-static void gen_rand_expr() {
-  switch (choose(3)) {
-  case 0: {gen_num(); break;}
-  case 1: {gen('('); gen_rand_expr(); gen(')'); break;}
-  case 2: {gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;}
-  default: {gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;}
+  FILE *fp = fopen("/tmp/.code.c", "w");
+  assert(fp != NULL);
+  fputs(code_buf, fp);
+  fclose(fp);
+
+  //int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+  int ret = system("gcc -w /tmp/.code.c -o /tmp/.expr");
+  if (ret != 0)
+  {
+    return 0;
   }
+
+  fp = popen("/tmp/.expr", "r");
+  assert(fp != NULL);
+
+  int result = 0x80000000;
+  fscanf(fp, "%d", &result);
+  pclose(fp);
+
+  if (result == x)
+  {
+    return 0;
+  }
+  return 1;
 }
 
-int main(int argc, char *argv[]) {
+static inline void gen_rand_expr(int flag)
+{
+  char *judge_zero = buf;
+  if (strlen(buf) > 0 && *(buf + strlen(buf) - 1) == '/')
+  {
+    judge_zero = buf + strlen(buf);
+  }
+
+  switch (choose(5))
+  {
+  case 0:
+    gen_blank();
+    gen_rand_expr(0);
+    break;
+  case 1:
+    gen_num();
+    break;
+  case 2:
+    gen_rand_expr(0);
+    gen_rand_op();
+    gen_rand_expr(0);
+    break;
+  case 3:
+    sprintf(buf + strlen(buf), "%s", "(");
+    gen_rand_expr(0);
+    sprintf(buf + strlen(buf), "%s", ")");
+    break;
+  case 4:
+    /*
+      if(choose(20) == 0)
+        sprintf(buf + strlen(buf), "%s", "!");
+      else
+        sprintf(buf + strlen(buf), "%s", "");
+      gen_rand_expr(0);
+      */
+    sprintf(buf + strlen(buf), "%s", "!");
+    gen_num();
+    break;
+  }
+  //}
+
+  if (judge_zero != buf)
+  {
+    if (check_expr(judge_zero, 0) == 0)
+    {
+		int size = sizeof(judge_zero);
+      memset(judge_zero, 0, size);
+      gen_rand_expr(0);
+      return;
+    }
+  }
+
+  if (flag == 1)
+  {
+    if (check_expr(buf, 0x80000000) == 0)
+    {
+      memset(buf, 0, sizeof(buf));
+      gen_rand_expr(1);
+      return;
+    }
+  }
+  return;
+}
+// TODO: if necessary, try to re-implement main function for better generation of expression
+
+int main(int argc, char *argv[])
+{
   int seed = time(0);
   srand(seed);
   int loop = 1;
-  if (argc > 1) {
+  if (argc > 1)
+  {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
 
+  for (i = 0; i < loop; i++)
+  {
+    buf[0] = '\0';
+
+    gen_rand_expr(1);
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -97,17 +204,17 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
     int result;
-    ret = fscanf(fp, "%d", &result);
+    fscanf(fp, "%d", &result);
     pclose(fp);
 
     printf("%u %s\n", result, buf);
-    memset(buf, 0, sizeof(buf));
   }
   return 0;
 }

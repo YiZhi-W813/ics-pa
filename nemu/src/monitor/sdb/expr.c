32b5b5d004ab43fc162f6f1e61231ed701dc5596
tracer-ics2023 <tracer@njuics.org>
2024-04-01 18:28:08 +0800

    >  compile NEMU xxxxxxxxx 赵治 Linux zz-Desktop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:28:08 up 23 min,  1 user,  load average: 0.57, 0.77, 0.75

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -362,9 +362,9 @@ word_t expr(char *e, bool *success) {
     }
 
 
-    for(int i = 0; i < tokens_len; i ++){ //初始化负数，负号在字符串的最前面或是负号的前面不是数字而后面是数字，则为负数
-	    if(tokens[i].type == TK_SUB && ((i > 0 && ( tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_RIGHT) 
-      && ( tokens[i+1].type == TK_NUM || tokens[i+1].type == TK_HEX )) || (i == 0))){
+    for(int i = 0; i < tokens_len; i ++){ //初始化负数，负号在字符串的最前面或是负号的前面不是数字/hex/reg/)而后面是数字/hex/reg/(，则为负数
+	    if(tokens[i].type == TK_SUB && ((i > 0 && ( tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_RIGHT && tokens[i-1].type != TK_REG) 
+      && ( tokens[i+1].type == TK_NUM || tokens[i+1].type == TK_HEX || tokens[i+1].type == TK_REG || tokens[i+1].type == TK_LEFT)) || (i == 0))){
         tokens[i].type = TK_NOTYPE; //判断出负号后把负号后一个token的全部内容向后移一位，最前面加个负号
         for(int j = 31; j > 0; j --){
           tokens[i+1].str[j] = tokens[i+1].str[j-1];
@@ -400,8 +400,8 @@ word_t expr(char *e, bool *success) {
 
     for(int i = 0 ; i < tokens_len ; i ++)  //初始化指针解引用
     {
-      if((tokens[i].type == TK_MUL && i > 0 && tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG && tokens[i+1].type == TK_NUM )
-        ||(tokens[i].type == TK_MUL && i > 0 && tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG && tokens[i+1].type == TK_HEX )
+      if((tokens[i].type == TK_MUL && i > 0 && tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG && tokens[i-1].type != TK_RIGHT && tokens[i+1].type == TK_NUM )
+        ||(tokens[i].type == TK_MUL && i > 0 && tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG && tokens[i-1].type != TK_RIGHT &&  tokens[i+1].type == TK_HEX )
         ||(tokens[i].type == TK_MUL && i == 0)){
         tokens[i].type = TK_NOTYPE;
         int addr = atoi(tokens[i+1].str);
